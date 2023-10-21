9ac63646043919eb5b7da6fd45b0b361ae2b882b
Raistaid <147325049+Raistaid@users.noreply.github.com>
2023-10-21 22:52:33 +0300

    Update Watch_out.py

--- Watch_out.py
+++ Watch_out.py
@@ -32,7 +32,7 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_a] and self.rect.x > 5:
             self.rect.x -= self.speed
-        if keys[K_d] and self.rect.x < win_width - 80:
+        if keys[K_d] and self.rect.x < win_width - 110:
             self.rect.x += self.speed
     
 class Enemy(GameSprite):
